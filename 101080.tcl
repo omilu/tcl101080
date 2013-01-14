716d76467667dcd3e8a478b19e30495537c18776
billy <omilu@ma.sdf.org>
2013-01-14 06:28:42 +0000

    added trace clear and trace add added time stamp

--- 101080.tcl
+++ 101080.tcl
@@ -1,8 +1,52 @@
 #!/usr/bin/tclsh
 package require Expect
 
-
+#globals
+set system_time [clock seconds];
+set ifr_ip "121.111.168.111 1080";
+set arduino_serial_port "/dev/ttyS18";
 set state on 
+set accumulator empty;
+set my_Quit false;
+
+#need to spawn the process globally so can refer to it in subprocess
+#will be handled with expect
+#spawn telnet "$ifr_ip";
+#set ifr_host $spawn_id;
+
+#the arduino serial looks just like a file
+#no need for expect
+#set arduino_serial [open $arduino_serial_port r+];
+#fconfigure $arduino_serial -mode "9600,n,8,1";
+#fconfigure $arduino_serial -buffering none
+
+#get a time stamp for the logfile name
+proc myGetTime {} {
+	set thisTime [clock seconds];
+	clock format $thisTime -format %Y%m%d%H%M%S
+}
+
+set filename [myGetTime];
+set logFileName [concat $filename.txt];
+set logFile [open "$logFileName" w]
+puts $logFile [concat [myGetTime] "101080 battery test start"];
+
+proc traceAdd {someText} {
+	global accumulator;
+	append accumulator $someText;
+}
+
+proc traceEmpty {} {
+	global accumulator;
+	set accumulator "_";
+}
+
+puts $accumulator
+traceEmpty
+traceAdd "shome shitty medssage";
+traceAdd "and more shit:"
+puts $accumulator
+
 
 
 
@@ -138,7 +182,7 @@ proc main {} {
 	ifrInit;
 
 	set x 0
-	while {$x < 3}	{
+	while {$x < 2}	{
 		puts "\nStarting a cycle"
 	txTest;
 	rxTest;
@@ -152,4 +196,26 @@ proc main {} {
 
 main;
 
+#troubleshooting and examples
+#declaring globals
+puts $ifr_ip;
+puts $state;
 
+#passing a global to a function with upvar
+proc glotest {state} {
+	upvar $state myvar;
+	puts $myvar;
+	set myvar off;
+	puts $myvar;
+}
+
+proc glotest2 {} {
+	global state;
+	puts "glotest2";
+	puts $state;
+}
+
+#exectuing the function
+glotest state;
+glotest2;
+puts "$state out of proc";
--- expect_practise.tcl
+++ expect_practise.tcl
@@ -1,7 +1,11 @@
 #!/usr/bin/expect
+#turns on helpful expect messages for troubleshotting
 exp_internal 1
+#set timeout short for developement default is 10sec
 set timeout 2
 send "Greetings what is your name?"
+#capture any reply, and match the whole respone
+#note the newline character
 expect -re {^.*.\n$}
 send "\nhi mutha fucka $expect_out(buffer);";
 send "\nwhat the $expect_out(0,string)\n";
